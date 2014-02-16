fdb488fd15a32c80347c3169318926ebd62d173c
johnyno <johnyno@gmail.com>
2014-02-17 01:08:21 +0200

    Connect 3.0 deprecation alert fix

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.use(express.methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
